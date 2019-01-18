3bf5394e27e2eca5cd442222b869798801ce400c
Gergo Endresz <k1721863@kingston.ac.uk>
2019-01-18 12:34:27 +0000

    Task class's attributes have been added and User.java has been created

--- Coursework/Task.java
+++ Coursework/Task.java
@@ -1,5 +1,12 @@
-class Task
+import java.util.LinkedList;
+public class Task
 {
     private boolean status;
+    private String title;
+    private String content;
+    private String dueDate;
+    private int priority;
+    private LinkedList<Task> subTasks;
+
     
 }
