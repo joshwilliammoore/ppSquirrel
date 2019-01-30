9ac8f97dc6260176d5a3b1cfd582971bc40e1e71
Gergo Endresz <k1721863@kingston.ac.uk>
2019-01-30 16:58:31 +0000

    Added Main folder for the central Main.java which we can use to test our code...

---
+++ Main/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import other.experimenting_with_java_ui.Experiment;
+
+/**
+ *
+ * @author Gregory
+ */
+public class Main {
+    public static void main(String[] args) {
+//		MainMenu mm = new MainMenu();
+//                
+//                ClassLoader loader = Main.class.getClassLoader();
+//                 System.out.println(loader.getResource("Main.java"));
+//                 System.out.println(System.getProperty("user.home"));
+                Experiment e = new Experiment();
+//                JFrame jf = new JFrame();
+//                jf.setLayout(new FlowLayout());
+//                 ImageIcon logoImage = new ImageIcon("C:\\GIT\\kickapoo\\squirrel\\view\\logo_png_100.png");
+//                JLabel logo = new  JLabel(logoImage);
+//                logo.setMinimumSize(new Dimension(500,500));
+//                 // logo.setIcon(logoImage);
+//                  //logo.setText(System.getProperty("user.dir"));
+//               jf.add(logo);
+//               jf.setMinimumSize(new Dimension(1000,1000));
+//               jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//               //jf.pack();
+//               jf.setVisible(true);
+//               System.out.println(System.getProperty("java.class.path")); 
+//                
+                
+	}
+    
+}
