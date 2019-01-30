1e017474cabbb1f3ec06b3395a4dd896c630f555
Gergo Endresz <k1721863@kingston.ac.uk>
2019-01-30 18:54:00 +0000

    Reverted the inheritance between Task and Subtask. Task has more functionalitites, so it has become the child of Subtask...also generated getters and setters

--- model/SubTask.java
+++ model/SubTask.java
@@ -1,9 +1,82 @@
-public class subtasks extends taskList
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class SubTask
 {
-    private String subtask;
-    private boolean complete;
-    private String[] subtasks;
+    private int id;
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Date createdDate;
+    private Date modifiedDate;
+    private int priority;
+    private boolean completed;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+   
+   
 }
