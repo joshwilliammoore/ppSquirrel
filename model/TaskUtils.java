90902148d343030c7671b03b9e2e2fb26778c871
Gergo Endresz <k1721863@kingston.ac.uk>
2019-02-01 12:53:23 +0000

    Added first filter function and the skeleton for the others.

--- model/TaskUtils.java
+++ model/TaskUtils.java
@@ -9,7 +9,50 @@ package model;
  *
  * @author joshw
  */
+import java.util.ArrayList;
+import java.util.Date;
 public class TaskUtils {
+    public static ArrayList<Task> searchFor(TaskList tl, User searchPhrase)
+    {
+        ArrayList<Task> results = new ArrayList<>();
+        
+       for(Task t : tl.getTasks())
+       {
+           if (t.getCreator().getUserName().equals( searchPhrase.getUserName())){
+               results.add(t);
+           }
+       
+       }
+      
+        
+        return results;
+    }
     
+    public static ArrayList<Task> searchFor(TaskList tl, Date searchPhrase)
+    {
+        ArrayList<Task> results = new ArrayList<>();
+        
+       for(Task t : tl.getTasks())
+       {
+         //comparision logic goes here
+       }
+      
+        
+        return results;
+    }
+    
+    
+        public static ArrayList<Task> searchFor(TaskList tl, int searchPhrase)
+    {
+        ArrayList<Task> results = new ArrayList<>();
+        
+       for(Task t : tl.getTasks())
+       {
+         //comparision logic goes here
+       }
+      
+        
+        return results;
+    }
     
 }
