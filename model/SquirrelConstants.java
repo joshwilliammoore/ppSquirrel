c8ecb97018d4cdfa713f491c82b268c42f59aac4
Gergo Endresz <k1721863@kingston.ac.uk>
2019-02-01 11:54:29 +0000

    Added SuirrelConstants class

---
+++ model/SquirrelConstants.java
@@ -0,0 +1,16 @@
+/*
+ 
+    Here we should add constants that we will use throughout the application, such as colours, element sizes, file references...etc.
+
+ */
+package model;
+
+/**
+ *
+ * @author Gregory
+ */
+class SquirrelConstants {
+    //reference from outside the class SquirrelConstants.H1SIZE !!!
+    public static final int H1SIZE = 31;
+    public static int taskID = 0;
+}
