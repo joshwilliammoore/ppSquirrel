c2479ba4bae279a217c3f4ec83aaea194e2c7d00
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-13 18:14:21 +0000

    Updated so that the initial layout can be tested....

--- Main/Main.java
+++ Main/Main.java
@@ -10,23 +10,16 @@ import model.Task;
 import model.SubTask;
 import model.TaskList;
 import java.util.Date;
+import view.MainContainer;
 /**
  *
  * @author Gregory
  */
 public class Main {
     public static void main(String[] args) {
-//		
-        //Experiment e = new Experiment();
-        //parameters for creating a task
-        //Date dueDate, int priority, String title, String description
-            Date dd = new Date(System.currentTimeMillis()+1000*3600*24);
-         Task task1 = new Task(dd, 2, "First demo task", "You should do it asap");       
-         Date dd2 = new Date(task1.getDueDate().getTime()-1000*3600);
-         SubTask subTask1 = new SubTask(dd2, 3, "First subtask", "You should do it a bit sooner");      
-         TaskList tl = new TaskList();
-         tl.addTask(task1);
-         task1.addSubTask(subTask1);
-	}
+        MainContainer mainWindow = MainContainer.getInstance();
+        mainWindow.setVisible(true);
+    
+    }
     
 }
