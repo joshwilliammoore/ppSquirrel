f52d4cb9357e04263c8d8ab8cb85a576b4c92f14
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-13 18:43:12 +0000

    Added package definition to irrelevant file.

--- other/refactored/Main.java
+++ other/refactored/Main.java
@@ -1,3 +1,5 @@
+package other.refactored;
+
 public class Main	 	       	  	 	        	      	   	
 {	 	       	  	 	        	      	   	
   public static void main(String[] args)	 	       	  	 	        	      	   	
