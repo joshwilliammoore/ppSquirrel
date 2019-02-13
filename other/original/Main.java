06cfe5ad4c065dd6cf42d33df82bd6e74bbf28c6
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-13 18:43:22 +0000

    Added package definition to irrelevant file.

--- other/original/Main.java
+++ other/original/Main.java
@@ -1,3 +1,4 @@
+package other.original;
 public class Main	 	       	  	 	        	      	   	
 {	 	       	  	 	        	      	   	
   public static void main(String[] args)	 	       	  	 	        	      	   	
