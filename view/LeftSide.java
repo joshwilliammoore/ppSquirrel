73b9b6988ad85c490489257c666b1faebaf866db
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-13 15:35:13 +0000

    Has made RightSide and LeftSide singleton classes.

--- view/LeftSide.java
+++ view/LeftSide.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package view;
-
+import javax.swing.JPanel;
 /**
  *
  * @author Regory Gregory
  */
-public class LeftSide {
+public class LeftSide extends JPanel{
     
+    private static LeftSide instance;
+    private LeftSide()
+    {
+        super();
+    }
+    
+    //making it a singleton class
+    public static LeftSide getInstance()
+    {
+    if(instance==null)
+    {
+        instance = new LeftSide();
+    }
+    return instance;
+    }
 }
--- view/RightSide.java
+++ view/RightSide.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package view;
-
+import javax.swing.JPanel;
 /**
  *
  * @author Regory Gregory
  */
 public class RightSide extends JPanel{
+    private static RightSide instance;
+    private RightSide()
+    {
+        super();
+    }
     
+    //making it a singleton class
+    public static  RightSide getInstance()
+    {
+    if(instance==null)
+    {
+        instance = new RightSide();
+    }
+    return instance;
+    }
 }
