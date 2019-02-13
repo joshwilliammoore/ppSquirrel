45ed02efc0d16a85b34980a34b8e16077ad39bbf
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-13 15:23:27 +0000

    Updated constants and added getters and setters (where applicable).

--- model/SquirrelConstants.java
+++ model/SquirrelConstants.java
@@ -9,8 +9,48 @@ package model;
  *
  * @author Gregory
  */
-class SquirrelConstants {
+public class SquirrelConstants {
+
+    
+    private static final int H1SIZE = 31;
+    private static int taskID = 0;
+    private static final int mainContainerWidth = 1000;
+    private static final int mainContainerHeight=1000;
+    private static final int leftSideWidth = 300;
+    private static final int rightSideWith = 700;
+    private static final int innerPaneHeight = 1000;
+    
+    
+    public static int getTaskID() {
+        return taskID;
+    }
+
+    public static int getH1SIZE() {
+        return H1SIZE;
+    }
+
+    public static int getMainContainerWidth() {
+        return mainContainerWidth;
+    }
+
+    public static int getMainContainerHeight() {
+        return mainContainerHeight;
+    }
+
+    public static int getLeftSideWidth() {
+        return leftSideWidth;
+    }
+
+    public static int getRightSideWith() {
+        return rightSideWith;
+    }
+
     //reference from outside the class SquirrelConstants.H1SIZE !!!
-    public static final int H1SIZE = 31;
-    public static int taskID = 0;
+    public static int getInnerPaneHeight() {
+        return innerPaneHeight;
+    }
+    public static void setTaskID(int taskID) {
+        SquirrelConstants.taskID = taskID;
+    }
+
 }
