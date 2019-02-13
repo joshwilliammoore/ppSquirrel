cbb42e29b4b5692908a9bd4aa1d91caf64ed296f
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-13 20:24:34 +0000

    Made it a singleton plus based on SquirrelConstants and a loop created the menu.

--- view/Menu.java
+++ view/Menu.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package view;
-
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import model.SquirrelConstants;
 /**
  *
  * @author Regory Gregory
  */
-public class Menu {
+public class Menu extends JPanel{
+    private static Menu instance=null;
+    private Menu()
+    {
+    super();
+    this.setLayout(new GridLayout(0,1));
+    String[] menuEntries = SquirrelConstants.getMenuEntries();
     
+    for(String m : menuEntries)
+    {
+        MenuEntry temp = new MenuEntry(m);
+        this.add(temp);
+    }
+    
+    
+    }
+    public static Menu getInstance()
+    {
+    if(instance==null)
+    {
+    instance = new Menu();
+    }
+    return instance;
+    
+    }
 }
