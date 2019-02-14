b201ff7107f7e78ef0aa0468cd214271922ce4a2
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-14 19:42:41 +0000

    Added horizontal panels which can contain later on the each screen's contents...etc

--- view/RightSide.java
+++ view/RightSide.java
@@ -5,6 +5,9 @@
  */
 package view;
 import model.SquirrelConstants;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
 /**
  *
  * @author Regory Gregory
@@ -16,18 +19,41 @@ public class RightSide extends GeneralPanel{
         super(SquirrelConstants.getRightSideWidth(), SquirrelConstants.getInnerPaneHeight());
       
         this.setBackground(SquirrelConstants.getRightSideBg());
+        
+ 
+        int size = SquirrelConstants.getHorizontalPaneHeight().length;
+        int[] hHeights = SquirrelConstants.getHorizontalPaneHeight();
+        String[] hColors = SquirrelConstants.getHorizontalPaneColor();
+        int offset=0;
+        for(int i =0; i<size; i++ )
+        {
+        JPanel temp = new JPanel();
+        temp.setBackground(Color.decode(hColors[i]));
+        temp.setPreferredSize(new Dimension(700, hHeights[i]));
+        
+        this.gbc.gridx=0;
+        
+        this.gbc.gridy= offset;
+        
+        this.gbc.gridheight=hHeights[i]/10;
+        this.add(temp, this.gbc);
+        offset+=hHeights[i]/10;
+        }
+       
+        
      
     }
+    
+        //making it a singleton class
+
     public static  RightSide getInstance()
     {
     if(instance==null)
     {
         instance = new RightSide();
     }
-//    return instance;
-//        instance = new RightSide();
+
         return instance;
     }
-    //making it a singleton class
    
 }
