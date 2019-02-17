638b557e4f7670053527f75f62e6e6c733d7b943
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-17 20:16:36 +0000

    Added TaskList creator functionality. Save doesn't work, nor has it been tested.

--- Controller/ActionButtonController.java
+++ Controller/ActionButtonController.java
@@ -7,6 +7,15 @@ package Controller;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JOptionPane;
+import java.awt.Component;
+import javax.swing.JComponent;
+import java.lang.reflect.Field;
+
+import view.RightSideElements.WorkingArea;
+import view.UIElements.GetUIContent;
+import model.TaskList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 /**
  *
  * @author Regory Gregory
@@ -24,7 +33,58 @@ public class ActionButtonController implements ActionListener{
                    ContentLoader.loadContent(command, null);
                 break;
             case "SAVE":
-
+                        switch(subCommands[1])
+                        {
+                            case "TASKLIST":
+                                Component[] elements = WorkingArea.getInstance().getComponents();
+                                TaskList tl = new TaskList();
+                                Class tlClass = tl.getClass();
+                                for(Component c : elements){
+                                    if(c instanceof GetUIContent)
+                                    {
+                                        
+                                        GetUIContent content = (GetUIContent) c;
+                                        String label = content.getLabel();
+                                        String value = content.getContent();
+                                      
+                                        try
+                                        {
+                                              if(label.equals("dateDue"))
+                                        {
+                                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                                        Date dueDate = dateFormat.parse(value);
+                                        Field f = tlClass.getField(label);
+                                        f.set(tl, dueDate);
+                                        } else {
+                                        Field f = tlClass.getField(label);
+                                        f.set(tl, value);
+                                              
+                                              }
+                                            
+                                            
+                                       
+                                        }catch(Exception ex)
+                                        {
+                                            ex.printStackTrace();
+                                        }
+                                        tl.setComplete(false);
+                                        tl.setDateCreated(new Date());
+                                        tl.setDateModified(new Date());
+                                        
+                                        
+                                        DataHandler.saveTaskList(tl);    
+                                    
+                                    }
+                                }
+                                break;
+                            case "TASK":
+                                break;
+                            case "SUBTASK":
+                                break;
+                        
+                        
+                        
+                        }
                 break;
             case "EDIT":
                 break;
