1cd8b6dc5064e1eb43184f5c0057d8d0c04202e8
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-17 18:17:41 +0000

    Reorganised the directory structure + made it compatible with the new ActionButtonController and ContentLoader classes + added date field. It is only text now, will have to do it later.

---
+++ view/UIElements/AddTaskListForm.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.UIElements;
+import view.UIElements.AddTaskForm;
+import java.awt.Component;
+import javax.swing.JComboBox;
+
+
+import javax.swing.JLabel;
+/**
+ *
+ * @author Regory Gregory
+ */
+public class AddTaskListForm extends AddTaskForm{
+    private static AddTaskListForm instance = null;
+    
+    protected AddTaskListForm(){
+        
+        super();
+       
+        
+        JLabel labelManager = new JLabel("Manager for the "+this.getTag());
+        
+        this.add(labelManager);
+        
+        
+        String[] manager = {"John", "Fred", "Sally", "Paul", "Josh"};
+        JComboBox managerDropdown = new JComboBox(manager);
+        this.add(managerDropdown);
+    }
+    
+    public static AddTaskListForm getInstance()
+    {
+        if(instance==null) instance = new AddTaskListForm();
+        return instance;
+    
+    }
+    
+    
+}
