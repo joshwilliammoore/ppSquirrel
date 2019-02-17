6cb5a3e178b5d97ee7bd698ac50c546002a01b5c
k1721863 <k1721863@PPRLRC02-013.kuds.kingston.ac.uk>
2019-02-17 22:23:10 +0000

    Added Tasklist loader and saver. Haven't tested it yet, probably needs debugging...

--- Controller/DataHandler.java
+++ Controller/DataHandler.java
@@ -4,12 +4,22 @@
  * and open the template in the editor.
  */
 package Controller;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import model.TaskList;
+import model.SquirrelConstants;
+import java.util.ArrayList;
 /**
  *
  * @author Regory Gregory
  */
 public class DataHandler {
+    
+    private static String s = System.getProperty("file.separator");
+    private static String dirPath = "."+s+SquirrelConstants.getSaveDir();
     public static TaskList[] loadTasklists()
     {
      TaskList[] loaded = new TaskList[0];
@@ -17,16 +27,72 @@ public class DataHandler {
      return loaded;
     }
     
-    public static boolean saveTaskList(TaskList tl)
+    public static boolean saveTaskList(TaskList taskList)
     {
         
-        return false;
+        
+        String filePath = dirPath+s+"taskList_id-"+taskList.getID();
+        File newDir = new File(dirPath);
+        File newFile = new File(filePath);
+        boolean success = false;
+        try
+        {   if(!newDir.exists())
+            {
+             newDir.mkdirs();
+
+            }
+            newFile.createNewFile();
+            
+            FileOutputStream fileOut = new FileOutputStream(newFile);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            objectOut.writeObject(taskList);
+            success = true;
+            
+            
+            
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return success;
     }
     
-    public static TaskList loadTasklist()
+    public static TaskList loadTasklist(String filename)
     {
-     TaskList loaded = new TaskList();
+        TaskList loaded = new TaskList();
+        
+        String dirPath = "."+s+SquirrelConstants.getSaveDir();
+        String filePath = dirPath+s+filename;
+        File newFile = new File(filePath);
+        try
+        {
+            
+            
+            FileInputStream fileIn = new FileInputStream(newFile);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+           loaded = (TaskList) objectIn.readObject();
+            
+            
+            
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return loaded;
      
-     return loaded;
+    }
+    
+    public static ArrayList<TaskList> loadTaskLists()
+    {
+        File saveDir = new File(dirPath);
+        File[] fileList = saveDir.listFiles();
+        ArrayList<TaskList> taskLists = new ArrayList<>();
+        
+        for(File f : fileList)
+        {
+        
+        taskLists.add(loadTasklist(f.getName()));
+        }
+    return taskLists;
     }
 }
