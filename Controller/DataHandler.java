a12a8f98c286db12fba0902087e1b96a1735c8f0
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-17 18:12:12 +0000

    This class will be responsible for saving and loading Tasklists and other settings. Currently has nothing in it.

---
+++ Controller/DataHandler.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+import model.TaskList;
+/**
+ *
+ * @author Regory Gregory
+ */
+public class DataHandler {
+    public static TaskList[] loadTasklists()
+    {
+     TaskList[] loaded = new TaskList[0];
+     
+     return loaded;
+    }
+    
+    public static boolean saveTaskList()
+    {
+        
+        return false;
+    }
+    
+    public static TaskList loadTasklist()
+    {
+     TaskList loaded = new TaskList();
+     
+     return loaded;
+    }
+}
