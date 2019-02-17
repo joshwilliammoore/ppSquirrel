eadc7c5a01f66485f50f0df3327e885fc5901660
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-17 18:11:40 +0000

    This class is responsible for updating the rightSide. It only updates the UI. It is called bu ActionButtonConroller but also from the menu directly. Might be a bit confusing, needs refactoring later.

---
+++ Controller/ContentLoader.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+import javax.swing.JOptionPane;
+import model.TaskList;
+import view.RightSideElements.ActionArea;
+import view.RightSideElements.ActionBar;
+import view.UIElements.AddTaskListForm;
+/**
+ *
+ * @author Regory Gregory
+ */
+public class ContentLoader {
+    
+    public static void loadContent(String text, Object param)
+    {
+
+        String[] subCommands = text.split(":");
+      
+            switch (subCommands[0])
+                {
+            case "HOME":
+                break;
+            case "TASKLISTS" :
+                TaskList[] taskLists = DataHandler.loadTasklists();
+                //here, you should load tasklists or if there is no available, 
+                //then it should say "There are no added tasks yet. Click "add" to create your first TaskList
+                ActionBar.TaskListBar();
+                break;
+            case "TASK" :
+                break;    
+            case "SETTINGS":
+            break;
+            case "NEW":
+
+                ActionBar.addNewBar(subCommands[1]);
+                ActionArea.getInstance().reFresh(AddTaskListForm.getInstance());
+                          
+                
+                break;
+            case "EXIT": System.exit(0);
+                break;
+            default: break;
+
+                }
+        
+    }
+    
+    
+    
+    
+    
+}
