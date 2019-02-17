a54122576ba0e7f4d1543f2fcb864290a757c982
Regory Gregory <Regory Gregory@10.130.13.187>
2019-02-17 18:14:35 +0000

    Refactored the "subtasks" to "children". Makes more sense.

--- model/Task.java
+++ model/Task.java
@@ -3,26 +3,26 @@ import java.util.ArrayList;
 import java.util.Date;
 public class Task extends SubTask
 {
-    private ArrayList<SubTask> subTasks;
+    private ArrayList<SubTask> children;
 
     public Task(Date dueDate, int priority, String title, String description)
     {
         super(dueDate, priority, title, description);
-        this.subTasks = new ArrayList<>();
+        this.children = new ArrayList<>();
     
     }
 
  
     public void addSubTask(SubTask st){
-        this.subTasks.add(st);
+        this.children.add(st);
     }
     
     public SubTask getSubtask(int Position){
-        return this.subTasks.get(Position);
+        return this.children.get(Position);
     }
     
     public ArrayList<SubTask> getSubtasks(){
-        return this.subTasks;
+        return this.children;
     }
     
 
