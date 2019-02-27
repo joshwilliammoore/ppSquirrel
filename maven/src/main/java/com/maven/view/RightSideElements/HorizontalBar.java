a88f477f75d03002a48ddda734785deb29c2e370
= <=>
2019-02-27 18:43:37 +0000

    Converting to maven...

---
+++ maven/src/main/java/com/maven/view/RightSideElements/HorizontalBar.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maven.view.RightSideElements;
+import com.maven.model.SquirrelConstants;
+import javax.swing.JPanel;
+import javax.swing.JComponent;
+
+
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Component;
+
+import com.maven.view.ReFreshable;
+/**
+ *
+ * @author Regory Gregory
+ */
+public class HorizontalBar extends JPanel{
+    private int pWidth = SquirrelConstants.getRightSideWidth();
+    private int pHeight;
+    private Dimension dim;
+    private GridBagConstraints gbc; 
+    private static HorizontalBar instance = null;
+
+    protected HorizontalBar()
+    {
+        super();
+   
+        this.setLayout(new GridBagLayout());
+        this.gbc = new GridBagConstraints();
+    }
+
+    public int getpWidth() {
+        return pWidth;
+    }
+
+    public void setpWidth(int pWidth) {
+        this.pWidth = pWidth;
+    }
+
+    public int getpHeight() {
+        return pHeight;
+    }
+
+    public void setpHeight(int pHeight) {
+        this.pHeight = pHeight;
+    }
+
+    
+    
+    public Dimension getDim() {
+        return dim;
+    }
+
+    public void setDim(Dimension dim) {
+        this.dim = dim;
+    }
+
+    public GridBagConstraints getGbc() {
+        return gbc;
+    }
+
+    public void setGbc(GridBagConstraints gbc) {
+        this.gbc = gbc;
+    }
+    public static HorizontalBar getInstance()
+    {
+    if(instance == null) instance = new HorizontalBar();    
+    return instance;
+    }
+  
+    public static void reFresh(Component c)
+    {
+    instance.removeAll();
+    instance.add(c);    
+    instance.revalidate();
+    instance.repaint();
+    }
+    
+    
+}
---
+++ maven/src/main/java/com/maven/view/RightSideElements/MessageBar.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maven.view.RightSideElements;
+
+import java.awt.Color;
+import com.maven.view.RightSideElements.HorizontalBar;
+import java.awt.Dimension;
+
+/**
+ *
+ * @author Regory Gregory
+ */
+public class MessageBar extends HorizontalBar{
+    private static MessageBar instance = null;
+    
+    protected MessageBar()
+    {
+        super();
+          this.setDim(new Dimension(700, 150));
+        this.setSize(this.getDim());
+        this.setPreferredSize(this.getDim());
+        this.setBackground(Color.ORANGE);
+
+    
+    }
+    
+    public static MessageBar getInstance()
+    {
+    if(instance == null) instance = new MessageBar();
+    
+    return instance;
+    }
+}
